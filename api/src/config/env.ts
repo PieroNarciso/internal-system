ba32b35b93f12ef75db5adf83202874fd01e6897
Piero Narciso <pieronarciso988@gmail.com>
2021-05-24 18:36:28 -0500

    Express session setup

--- api/src/config/env.ts
+++ api/src/config/env.ts
@@ -1,3 +1,7 @@
 export const {
+  NODE_ENV = 'development',
   DB_URI = '',
+  SALT_ROUNDS = 10,
+  SESSION_NAME = 'suid',
+  SECRET_KEY = 'secret',
 } = process.env;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import session from 'express-session';
 
 import { globalRouter } from '@/api/routes';
+import { SECRET_KEY, NODE_ENV, SESSION_NAME } from '@/config/env';
 import db from '@/config/db';
 
 const PORT = 8080;
@@ -9,9 +11,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+  name: SESSION_NAME,
+  secret: SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: NODE_ENV == 'development'? false : true,
+  }
+}));
+
 /** DB Connection */
 db
-  .then((_) => console.log('DB Connected'))
+  .then(() => console.log('DB Connected'))
   .catch((err) => console.error(err));
 
 /** Global Routing */
