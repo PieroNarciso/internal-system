e83e7cc7f65d2b360f73523e2d59d70393c9c7d5
Piero Narciso <pieronarciso988@gmail.com>
2021-07-08 12:51:11 -0500

    Update controller

---
+++ api/src/api/controllers/OrdenServicioController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+
+import { OrdenServicio } from '@/api/models/OrdenServicio';
+import { getRepository } from 'typeorm';
+
+
+export const createOrdenServicio = async (req: Request, res: Response) => {
+  try {
+    const [orden] = getRepository(OrdenServicio).create([{ ...req.body }]);
+    await orden.save();
+    return res.status(201).send(orden);
+  } catch(err) {
+    return res.status(500).send(err);
+  }
+}
+
+export const getOrdenServicioById = async (req: Request, res: Response) => {
+  try {
+    const orden = await getRepository(OrdenServicio).findOne(req.params.ordenId);
+    if (orden) {
+      return res.status(200).send(orden);
+    }
+    return res.status(404).send({ msg: 'No encontrado' });
+  } catch(err) {
+    return res.status(500).send(err);
+  }
+}
+
+/**
+  * Solo se puede hacer update a `estado` y `numOrden`
+  */
+export const updateOrdenServicioById = async (req: Request, res: Response) => {
+  const { estado, numOrden } = req.body;
+  try {
+    const orden = await getRepository(OrdenServicio).findOne(req.params.ordenId);
+    if (orden) {
+      orden.estado = estado? estado: orden.estado;
+      orden.numOrden = numOrden? numOrden: orden.numOrden;
+      await orden.save();
+      return res.status(200).send(orden);
+    }
+    return res.status(404).send({ msg: 'No encontrado' });
+
+  } catch(err) {
+    return res.status(500).send(err);
+  }
+}
