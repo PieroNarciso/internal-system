c5ad4697ef41e128f1a74c2fec97794edc4909d4
Piero Narciso <pieronarciso988@gmail.com>
2021-08-09 16:28:39 -0500

    Migrate openapi 3.0.0 with typescript

--- api/src/docs/index.ts
+++ api/src/docs/index.ts
@@ -1,5 +1,174 @@
-import { resolve } from 'path';
-import YAML from 'yamljs';
+import { OpenAPIV3 } from 'openapi-types';
 
 
-export const swaggerDocument = YAML.load(resolve(__dirname, 'specs.yaml'));
+const components: OpenAPIV3.ComponentsObject = {
+  schemas: {
+    Estado: {
+      type: 'string',
+      enum: ['usuario', 'admin'],
+    },
+    TipoNombre: {
+      type: 'string',
+      enum: ['despacho', 'produccion']
+    },
+    Role: {
+      type: 'string',
+      enum: ['usuario', 'admin']
+    },
+    RegisterUser: {
+      type: 'object',
+      properties: {
+        username: {
+          type: 'string',
+        },
+        password: {
+          type: 'string'
+        },
+        email: {
+          type: 'string'
+        },
+      }
+    },
+    LoginUser: {
+      type: 'object',
+      properties: {
+        username: {
+          type: 'string',
+        },
+        password: {
+          type: 'string'
+        }
+      }
+    },
+    LoginUserResponse: {
+      type: 'object',
+      properties: {
+        uuid: {
+          type: 'string',
+        },
+        username: {
+          type: 'string'
+        }
+      }
+    },
+    User: {
+      type: 'object',
+      properties: {
+        username: {
+          type: 'string',
+        },
+        email: {
+          type: 'string'
+        },
+        id: {
+          type: 'number',
+        },
+        uuid: {
+          type: 'string',
+        },
+        role: {
+          $ref: '#/components/schemas/Role'
+        }
+      }
+    }
+  }
+};
+
+const tags: OpenAPIV3.TagObject[] = [
+  { name: 'User' },
+];
+
+const paths: OpenAPIV3.PathsObject= {
+  '/user/login': {
+    post: {
+      summary: 'Login User',
+      tags: ['User'],
+      requestBody: {
+        required: true,
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/LoginUser'
+            }
+          }
+        }
+      },
+      responses: {
+        '200': {
+          description: 'User logged',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/LoginUserResponse'
+              }
+            }
+          }
+        }
+      }
+    },
+  },
+  '/users': {
+    post: {
+      summary: 'Create an User',
+      tags: ['User'],
+      requestBody: {
+        required: true,
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/RegisterUser'
+            }
+          }
+        }
+      },
+      responses: {
+        '201': {
+          description: 'User created',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#components/schemas/User'
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  '/users/{uuid}': {
+    get: {
+      summary: 'Get user by uuid',
+      tags: ['User'],
+      parameters: [
+        { name: 'uuid', in: 'path', required: true }
+      ],
+      responses: {
+        '200': {
+          description: 'User found',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/User'
+              }
+            }
+          }
+        },
+        '404': {
+          description: 'User not found'
+        }
+      }
+    }
+  }
+};
+
+export const swaggerDocument: OpenAPIV3.Document = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Control Produccion RestAPI',
+    description: 'Control Produccion Documentacion',
+    version: '0.1.0'
+  },
+  tags,
+  components,
+  paths
+};
