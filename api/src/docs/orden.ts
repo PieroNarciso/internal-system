6c31e0925951fc528ec1e71c23a102bb52182d13
Piero Narciso <pieronarciso988@gmail.com>
2021-09-01 16:37:40 -0500

    Update `orden` docs
    * Create Orden
    * Add Item

--- api/src/docs/orden.ts
+++ api/src/docs/orden.ts
@@ -91,6 +91,46 @@ export const ordenComponents: OpenAPIV3.ComponentsObject['schemas'] = {
       }
     }
   },
+  ItemCreateBody: {
+    type: 'object',
+    properties: {
+      name: {
+        type: 'string',
+        description: 'Item name description',
+      },
+      totalDespachar: {
+        type: 'number',
+      },
+    }
+  },
+  HistoriaCreateBody: {
+    type: 'object',
+    properties: {
+      itemId: {
+        type: 'number'
+      },
+      tipoId: {
+        type: 'number'
+      },
+      peso: {
+        type: 'number'
+      }
+    }
+  },
+  OrdenCreateBody: {
+    type: 'object',
+    properties: {
+      numOrden: {
+        type: 'string',
+      },
+      items: {
+        type: 'array',
+        items: {
+          $ref: '#/components/schemas/ItemCreateBody',
+        }
+      }
+    },
+  }
 };
 
 export const ordenPaths: OpenAPIV3.PathsObject = {
@@ -108,6 +148,35 @@ export const ordenPaths: OpenAPIV3.PathsObject = {
                 items: {
                   $ref: '#/components/schemas/Orden'
                 }
+              }
+            }
+          }
+        },
+        '500': {
+          description: 'Server Error'
+        }
+      }
+    },
+    post: {
+      summary: 'Create Orden with Items',
+      tags: ['Orden'],
+      requestBody: {
+        required: true,
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/OrdenCreateBody'
+            }
+          }
+        }
+      },
+      responses: {
+        '201': {
+          description: 'Orden created successfully',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/Orden'
               }
             }
           }
@@ -138,6 +207,43 @@ export const ordenPaths: OpenAPIV3.PathsObject = {
         },
         '404': {
           description: 'Not Found',
+        }
+      }
+    }
+  },
+  '/ordenes/{ordenId}/items': {
+    post: {
+      summary: 'Add an Item to the Orden',
+      tags: ['Orden', 'Item'],
+      parameters: [
+        { name: 'ordenId', in: 'path', required: true }
+      ],
+      requestBody: {
+        required: true,
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/ItemCreateBody',
+            }
+          }
+        }
+      },
+      responses: {
+        '201': {
+          description: 'Successful added',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/Item'
+              }
+            }
+          }
+        },
+        '404': {
+          description: 'Not Found'
+        },
+        '500': {
+          description: 'Server Error'
         }
       }
     }
