60882003054362e6b9b1ff3d67483295afd9cece
Piero Narciso <pieronarciso988@gmail.com>
2021-09-06 11:14:49 -0500

    Empresa implementation
    * EmpresaService and Controller

--- api/src/controllers/EmpresaController.ts
+++ api/src/controllers/EmpresaController.ts
@@ -1,38 +1,24 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
-import { Empresa } from '@/models/Empresa';
-import { FindOneOptions, getRepository } from 'typeorm';
+import EmpresaService from '@/services/EmpresaService';
 
-export const createEmpresa = async (req: Request, res: Response) => {
+export const createEmpresa = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const [empresa] = getRepository(Empresa).create([{ ...req.body }]);
-    await empresa.save();
+    const empresa = await EmpresaService.create({
+      razonSocial: req.body.razonSocial,
+      ruc: req.body.ruc
+    });
     return res.status(201).send(empresa);
   } catch (err) {
-    return res.status(500).send(err);
+    next(err);
   }
 };
 
-/**
- * Recibe como parametro `empresaId` que es la Pk de la instancia a buscar
- * Querys:
- *   -- ordenes: boolean Agregar las ordenes de la empresa o no
- */
-export const getEmpresaById = async (req: Request, res: Response) => {
-  const options: FindOneOptions<Empresa> = req.query.ordenes === 'true'
-    ? { relations: ['ordenes'] }
-    : {};
+export const getEmpresaById = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const empresa = await getRepository(Empresa).findOne(
-      req.params.empresaId,
-      options
-    );
-
-    if (empresa) {
-      return res.status(200).send(empresa);
-    }
-    return res.status(404).send({ msg: 'No encontrado' });
+    const empresa = await EmpresaService.findById(parseInt(req.params.empresaId));
+    return res.status(200).send(empresa);
   } catch (err) {
-    return res.status(500).send(err);
+    next(err);
   }
 };
