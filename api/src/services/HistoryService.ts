359a7f0f2befbb3257b5e57d44e8ea6af9cc02f0
Piero Narciso <pieronarciso988@gmail.com>
2021-09-08 21:22:16 -0500

    Added History service
    * Create instance method

--- api/src/services/HistoryService.ts
+++ api/src/services/HistoryService.ts
@@ -4,11 +4,15 @@ import { Historia } from '@/models/Historia';
 
 class HistoryService {
   public async create(payload: {
+    ordenId: number,
+    itemId: number,
     tipoId: number;
     peso: number;
   }): Promise<Historia> {
     try {
       const history = getRepository(Historia).create({
+        orden: { id: payload.ordenId },
+        item: { id: payload.itemId },
         tipo: { id: payload.tipoId },
         peso: payload.peso,
       });
