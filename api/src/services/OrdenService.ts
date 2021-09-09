674c51f97c563a3c9cbc953bfa0770decf206811
Piero Narciso <pieronarciso988@gmail.com>
2021-09-08 21:21:42 -0500

    Refactor with Item Service
    * Refactoring Item bulkCreate

--- api/src/services/OrdenService.ts
+++ api/src/services/OrdenService.ts
@@ -3,6 +3,7 @@ import { OrdenServicio } from '@/models/OrdenServicio';
 import { Item } from '@/models/Item';
 import { Estado } from '@/types';
 import { Empresa } from '@/models/Empresa';
+import ItemService from './ItemService';
 
 class OrdenService {
   public async create(payload: {
@@ -19,20 +20,12 @@ class OrdenService {
       });
       await orden.save();
       if (payload.items) {
-        for (const itemAttrs of payload.items) {
-          try {
-            const item = getRepository(Item).create({
-              nombre: itemAttrs.name,
-              totalDespachar: itemAttrs.totalDespachar,
-            });
-            await item.save();
-          } catch (err) {
-            console.log(err);
-            continue;
-          }
-        }
+        orden.items = await ItemService.bulkCreate(
+          payload.items.map((item) => {
+            return { ...item, ordenId: orden.id };
+          })
+        );
       }
-      await orden.empresa.reload();
       return orden;
     } catch (err) {
       throw new Error(err);
@@ -41,7 +34,9 @@ class OrdenService {
 
   public async findAll(): Promise<OrdenServicio[]> {
     try {
-      const ordenes = await getRepository(OrdenServicio).find();
+      const ordenes = await getRepository(OrdenServicio).find({
+        relations: ['items'],
+      });
       return ordenes;
     } catch (err) {
       throw new Error(err);
@@ -67,6 +62,7 @@ class OrdenService {
     payload: {
       empresaId?: number;
       estado?: Estado;
+      numOrden?: string;
       items?: { id: number; name: string; totalDespachar: number }[];
     }
   ): Promise<boolean> {
@@ -78,6 +74,7 @@ class OrdenService {
       if (!orden) return false;
       if (payload.empresaId) orden.empresa = <Empresa>{ id: payload.empresaId };
       if (payload.estado) orden.estado = payload.estado;
+      if (payload.numOrden) orden.numOrden = payload.numOrden;
       if (payload.items) {
         for (const item of payload.items) {
           try {
@@ -92,6 +89,7 @@ class OrdenService {
           }
         }
       }
+      await orden.save();
       return true;
     } catch (err) {
       throw new Error(err);
