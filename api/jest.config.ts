cf4dcc0cbcb7299c7c95f4acf09f75ae0b9784f9
Piero Narciso <pieronarciso988@gmail.com>
2021-09-10 17:21:51 -0500

    TipoService unit tests

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -2,6 +2,8 @@ import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
+  verbose: true,
+  collectCoverage: true,
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
--- api/src/services/__test__/TipoService.spec.ts
+++ api/src/services/__test__/TipoService.spec.ts
@@ -1,18 +1,49 @@
-import { getConnection, getRepository } from 'typeorm';
-import { dbConnection } from '../config/db';
+import { getConnection } from 'typeorm';
+import { dbConnection } from '../../config/db';
 
-import { Tipo } from '../models/Tipo';
+import { TipoNombre } from '../../types';
+import TipoService from '../TipoService';
 
 describe('Tipo Service unit tests', () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     await dbConnection();
   });
-  afterEach(async () => {
+  afterAll(async () => {
     await getConnection().close();
   });
+  afterEach(async () => {
+    await getConnection().synchronize(true);
+  });
 
-  test('Example', async () => {
-    const tipos = await getRepository(Tipo).find();
+  test('Tipo instances must be empty', async () => {
+    const tipos = await TipoService.findAll();
     expect(tipos.length).toBe(0);
+  });
+
+  test('Tipo instances must be one', async () => {
+    await TipoService.create({
+      tipoName: TipoNombre['DESPACHO'],
+    });
+    const tipos = await TipoService.findAll();
+    expect(tipos.length).toBe(1);
+  });
+
+  test('Tipo instances must be two', async () => {
+    await TipoService.create({ tipoName: TipoNombre['DESPACHO'] });
+    await TipoService.create({ tipoName: TipoNombre['PRODUCCION'] });
+    const tipos = await TipoService.findAll();
+    expect(tipos.length).toBe(2);
+  });
+
+  test('Tipo duplicated', async () => {
+    try {
+      await TipoService.create({ tipoName: TipoNombre['DESPACHO'] });
+      await TipoService.create({ tipoName: TipoNombre['DESPACHO'] });
+    } catch (err) {
+      expect(err).toBeDefined();
+    } finally {
+      const tipos = await TipoService.findAll();  
+      expect(tipos.length).toBe(1);
+    }
   });
 });
