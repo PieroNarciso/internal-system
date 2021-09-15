911d76802d10fb0b6df13fbec8e065204f085ce4
Piero Narciso <pieronarciso988@gmail.com>
2021-09-15 11:30:14 -0500

    Refactor tests in TipoServices

--- api/src/services/__tests__/TipoService.spec.ts
+++ api/src/services/__tests__/TipoService.spec.ts
@@ -3,50 +3,62 @@ import { dbConnection } from '../../config/db';
 
 import { TipoNombre } from '../../types';
 import TipoService from '../TipoService';
+import { Tipo } from '../../models/Tipo';
 
-describe('Tipo Service unit tests', () => {
+export const NUMBER_TIPO_INSTANCES_INITIALIZED = 2;
 
-  beforeAll(async () => {
-    await dbConnection();
+export const testsTipoInstancesInitialize = async () => {
+  await Tipo.create({
+    nombre: TipoNombre['DESPACHO'],
+  }).save();
+  await Tipo.create({
+    nombre: TipoNombre['PRODUCCION'],
+  }).save();
+};
+
+beforeAll(async () => {
+  await dbConnection();
+});
+
+afterAll(async () => {
+  await getConnection().close();
+});
+
+afterEach(async () => {
+  await getConnection().synchronize(true);
+});
+
+describe('Tipo Service create method', () => {
+  test('if Tipo instances must be one', async () => {
+    await TipoService.create({
+      tipoName: TipoNombre['DESPACHO'],
+    });
+    const tipos = await Tipo.find();
+    expect(tipos.length).toBe(1);
   });
 
-  afterAll(async () => {
-    await getConnection().close();
+  test('if Tipo is duplicated', async () => {
+    try {
+      await testsTipoInstancesInitialize();
+      await Tipo.create({ nombre: TipoNombre['DESPACHO'] }).save();
+    } catch (err) {
+      expect(err).toBeDefined();
+    } finally {
+      const tipos = await TipoService.findAll();
+      expect(tipos.length).toBe(NUMBER_TIPO_INSTANCES_INITIALIZED);
+    }
   });
+});
 
-  afterEach(async () => {
-    await getConnection().synchronize(true);
-  });
-
+describe('Tipo Service findAll method', () => {
   test('if Tipo instances must be empty', async () => {
     const tipos = await TipoService.findAll();
     expect(tipos.length).toBe(0);
   });
 
-  test('if Tipo instances must be one', async () => {
-    await TipoService.create({
-      tipoName: TipoNombre['DESPACHO'],
-    });
-    const tipos = await TipoService.findAll();
-    expect(tipos.length).toBe(1);
-  });
-
   test('if Tipo instances must be two', async () => {
-    await TipoService.create({ tipoName: TipoNombre['DESPACHO'] });
-    await TipoService.create({ tipoName: TipoNombre['PRODUCCION'] });
+    await testsTipoInstancesInitialize();
     const tipos = await TipoService.findAll();
-    expect(tipos.length).toBe(2);
-  });
-
-  test('if Tipo is duplicated', async () => {
-    try {
-      await TipoService.create({ tipoName: TipoNombre['DESPACHO'] });
-      await TipoService.create({ tipoName: TipoNombre['DESPACHO'] });
-    } catch (err) {
-      expect(err).toBeDefined();
-    } finally {
-      const tipos = await TipoService.findAll();  
-      expect(tipos.length).toBe(1);
-    }
+    expect(tipos.length).toBe(NUMBER_TIPO_INSTANCES_INITIALIZED);
   });
 });
