0387d9eaabdcfff3b3fba858c94362554e8ba084
Piero Narciso <pieronarciso988@gmail.com>
2021-09-24 11:07:35 -0500

    Login view migrated

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -2,12 +2,27 @@ import React, { Fragment, useState } from 'react';
 
 import { loginUser } from '@/store/user/user.thunks';
 import { useAppDispatch } from '@/hooks';
-import { Box, Flex, Input, InputGroup, FormControl, VStack, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Input,
+  InputGroup,
+  FormControl,
+  VStack,
+  Button,
+  FormLabel,
+  InputRightElement,
+  IconButton,
+} from '@chakra-ui/react';
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
 
 const Login: React.FC = () => {
   const dispatch = useAppDispatch();
+  const [showPassword, setShowPassword] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const showPasswordToggle = () => setShowPassword((prevState) => !prevState);
 
   const usernameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
@@ -38,29 +53,39 @@ const Login: React.FC = () => {
       <Box borderRadius="lg" borderWidth="1px" p="4">
         <form onSubmit={loginOnSubmit}>
           <VStack spacing="3">
-          <FormControl>
-            <InputGroup>
-              <Input
-                type="text"
-                placeholder="Nombre de usuario"
-                value={username}
-                onChange={usernameHandler}
-              />
-            </InputGroup>
-          </FormControl>
-          <FormControl>
-            <InputGroup>
-              <Input
-                type="password"
-                placeholder="Contraseña"
-                value={password}
-                onChange={passwordHandler}
-              />
-            </InputGroup>
-          </FormControl>
-          <Flex width="full" justifyContent="end">
-            <Button type="submit">Ingresar</Button>
-          </Flex>
+            <FormControl isRequired>
+              <FormLabel>Nombre de usuario</FormLabel>
+              <InputGroup>
+                <Input
+                  type="text"
+                  placeholder="Nombre de usuario"
+                  value={username}
+                  onChange={usernameHandler}
+                  required
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Contraseña</FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Contraseña"
+                  value={password}
+                  onChange={passwordHandler}
+                />
+                <InputRightElement>
+                  <IconButton
+                    onClick={showPasswordToggle}
+                    aria-label="Show password"
+                    icon={showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+                  />
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Flex width="full" justifyContent="end">
+              <Button type="submit">Ingresar</Button>
+            </Flex>
           </VStack>
         </form>
       </Box>
